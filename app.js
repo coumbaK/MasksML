9a17f4f75ae3c63a6bb879cf6b7f968600721194
Glitch (glitch-hello-website) <none>
2022-11-13 02:20:48 +0000

    🐔👟 Checkpoint ./index.html:270926/732 ./app.js:270926/1920

--- app.js
+++ app.js
@@ -8,12 +8,30 @@
 /* globals Vue, p5*/
 
 window.addEventListener("load", function () {
-  const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-  const detectorConfig = {
-  runtime: 'mediapipe', // or 'tfjs'
-  solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-  }
-  faceLandmarksDetection.createDetector(model, detectorConfig).then(() => {
-    console.log("detector created")
-  }) 
+  
+  
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('videoElement');
+
+
+if (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia) {
+              navigator.mediaDevices.getUserMedia({ video: true,audio:false })
+                .then(function (stream) {
+                  video.srcObject = stream;
+                console.log("VIDEO STARTED")
+                })
+                .catch(function (err) {
+                  console.log("Something went wrong!");
+                });}
+  
+//   const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+//   const detectorConfig = {
+//   runtime: 'mediapipe', // or 'tfjs'
+//   solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+//   }
+//   faceLandmarksDetection.createDetector(model, detectorConfig).then(() => {
+//     console.log("detector created")
+//   }) 
 });
