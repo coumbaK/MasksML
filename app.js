e7ac532d89ffbe674ca734a563b185dc15b97689
Glitch (glitch-hello-website) <none>
2022-11-15 17:48:51 +0000

    🌹🍰 Checkpoint ./app.js:270926/1593 ./index.html:270926/457 ./.glitch-assets:270926/348

--- app.js
+++ app.js
@@ -1,48 +1,15 @@
+let predictions = [];
+const video = document.getElementById('video');
 
-/**
+// Create a new facemesh method
+const facemesh = ml5.facemesh(video, modelLoaded);
 
- * Starter code
- * Data visualization
- */
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
 
-
-console.log("load a model?")
-tf.loadLayersModel('https://foo.bar/tfjs_artifacts/model.json').then(model => {
-  console.log("I have a model")
-})
-  
-
-/* globals Vue, p5*/
-
-window.addEventListener("load", function () {
-  
-  
-// JavaScript
-
-
-
-  
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var video = document.getElementById('videoElement');
-
-
-if (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia) {
-              navigator.mediaDevices.getUserMedia({ video: true,audio:false })
-                .then(function (stream) {
-                  video.srcObject = stream;
-                console.log("VIDEO STARTED")
-                })
-                .catch(function (err) {
-                  console.log("Something went wrong!");
-                });}
-  
-//   const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-//   const detectorConfig = {
-//   runtime: 'mediapipe', // or 'tfjs'
-//   solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-//   }
-//   faceLandmarksDetection.createDetector(model, detectorConfig).then(() => {
-//     console.log("detector created")
-//   }) 
+// Listen to new 'face' events
+facemesh.on('face', results => {
+  predictions = results;
 });
