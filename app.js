61b279a98a71f766e1ae86b0c56fce3365f0054c
Glitch (glitch-hello-website) <none>
2022-11-15 18:07:08 +0000

    🐯💘 Checkpoint ./index.html:270926/588 ./app.js:270926/7784 ./style.css:270926/26

--- app.js
+++ app.js
@@ -1,15 +1,92 @@
-let predictions = [];
-const video = document.getElementById('video');
+
+
+window.addEventListener("load", function () {
+  //------------------------------------------------------
+  //------------------------------------------------------
+  //------------------------------------------------------
+  //------------------------------------------------------
+  // VUE!!!
+  // Create a new vue interface
+
+  new Vue({
+    template: `<div id="app">
+	    
+         <video class="overlay" controls muted id="video" crossorigin="anonymous">
+      
+
+<!-- https://www.lvlt.org/thequarantinemonologues -->
+        <source src="https://cdn.glitch.global/f422c25d-a910-4374-8c72-f41291b2b254/youtuber.mp4?v=1668534362785" type="video/mp4">
+     
+      </video>
+      <div ref="canvasHolder"></div>		
+      
+		  
+  </div>`,
+
+    mounted() {
+      // Create P5 when we mount this element
+      const s = (p0) => {
+        p = p0;
+
+        (p.preload = () => {
+          // Any preloading
+        }),
+          (p.setup = () => {
+            p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+            p.colorMode(p.HSL, 360, 100, 100);
+            p.ellipseMode(p.RADIUS);
+          });
+
+        p.draw = () => {
+          // Draw something
+          p.circle(0, 0, 100)
+          
+        };
+
+        p.mouseClicked = () => {
+          // Mouse interaction
+        };
+      };
+
+      let p = undefined;
+      const CANVAS_WIDTH = 200;
+      const CANVAS_HEIGHT = 200;
+      // Create P5
+      const CANVAS_EL = this.$refs.canvasHolder;
+      CANVAS_EL.style.width = CANVAS_WIDTH + "px";
+      CANVAS_EL.style.height = CANVAS_HEIGHT + "px";
+      new p5(s, CANVAS_EL);
+      
+      
+//       Setup the video
+      let predictions = [];
+const video = document.getElementById("video");
+
+
+// When the model is loaded
+function modelLoaded() {
+  console.log("Model Loaded!");
+}
 
 // Create a new facemesh method
 const facemesh = ml5.facemesh(video, modelLoaded);
 
-// When the model is loaded
-function modelLoaded() {
-  console.log('Model Loaded!');
-}
+
 
 // Listen to new 'face' events
-facemesh.on('face', results => {
-  predictions = results;
+facemesh.on("face", (results) => {
+  // console.log("new face")
+  // predictions = results;
+  // console.log(predictions)
+});
+
+
+    },
+
+    // We will use your data object as the data for Vue
+    data() {
+       return {}
+    },
+    el: "#app",
+  });
 });
