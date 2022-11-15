7e8d95f2f306d32060b629c30f3a42509aa73bb4
Glitch (glitch-hello-website) <none>
2022-11-15 18:54:20 +0000

    🚓🍯 Checkpoint ./face.js:270926/726 ./a6-assignment.md:270926/475

---
+++ a6-assignment.md
@@ -0,0 +1,3 @@
+https://github.com/tensorflow/tfjs-models/tree/master/face-landmarks-detection#parameters-for-facelandmarksdetectionload
+
+map of all the points https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/mesh_map.jpg
--- face.js
+++ face.js
@@ -7,6 +7,8 @@
 * as a bounding box and also points *relative* to that box???
 **/
 
+EYE_CONTOUR_INDICES = []
+
 const LANDMARK_COUNT = 468
 
 class Face {
@@ -14,8 +16,24 @@ class Face {
 //     All faces have 468 points
     this.points = []
 for (var i = 0; i < LANDMARK_COUNT; i++) {
-  this.points[i] = new Vector2D(0,0)
+  let pt = new Vector2D(0,0)
+  pt.index = i
+  this.points[i] = pt
 }
+    
+// Group all the points into landmarks
+    this.sides = []
+    for (var i = 0; i < 2; i++) {
+      this.sides[i] = {
+        eyeContours: EYE_CONTOUR_INDICES[i].map(sideContours => {
+          // For each 
+          return sideContours.map(contour => {
+             return  this.points[index])
+          })
+         
+      }
+    }
+    
   }
   
   setTo(prediction) {
@@ -31,6 +49,7 @@ for (var i = 0; i < LANDMARK_COUNT; i++) {
   drawDebug(p) {
     this.points.forEach(pt => {
       p.circle(...pt, 4)
+      p.text(pt.index, ...pt)
     })
   }
 }
