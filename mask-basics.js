f8818993ca88098521f1a794d6e12321858222c0
Glitch (glitch-hello-website) <none>
2022-11-16 14:35:50 +0000

    ⭐️😋 Checkpoint ./mask-basics.js:270926/1567

--- mask-basics.js
+++ mask-basics.js
@@ -1,4 +1,4 @@
-/* globals allMasks  Vector2D, drawContour */
+/* globals allMasks  Vector2D, drawRibbon, drawContour, drawPoints */
 
 allMasks["basics"] = {
   title: "Example mask",
@@ -20,19 +20,53 @@ allMasks["basics"] = {
     
 //     face.mouth an array of contours, 
     // each of which is an array of Vecto2D
-    face.mouth.forEach((contour, index) => {
-      p.fill(index*50, 100, 50)
-      p.beginShape()
-      contour.forEach(pt => {
-        // p.circle(...pt, 4)
-        // p.text("❤️", ...pt)
-        p.vertex(...pt)
+//     face.mouth.forEach((contour, index) => {
+//       p.fill(index*50, 100, 50)
+//       p.beginShape()
+//       contour.forEach(pt => {
+//         // p.circle(...pt, 4)
+//         // p.text("❤️", ...pt)
+//         p.vertex(...pt)
+//       })
+      
+//       p.endShape()
+//     })
+    
+    // Using drawPoints
+    p.stroke(0, 100, 50)
+    p.noFill()
+    p.strokeWeight(3)
+    p.beginShape()
+    drawPoints(p,face.mouth[2].slice(12, 20), {
+      // close: true
+    })
+    p.vertex(...face.bottom)
+    p.endShape(p.CLOSE)
+    
+    drawContour(p, face.centerLine)
+    
+// SIDES!
+    face.sides.forEach((side, sideIndex) => {
+      
+      // Inside a side
+      // p.strokeWeight(4)
+      // p.circle(...side.eyeCenter, 5)
+      // p.strokeWeight(1)
+      side.face.forEach((contour, index) => {
+        let hue = (sideIndex*100 + 50*index)%360
+        p.fill(hue, 100, 50, 1)
+        drawContour(p, contour, {
+          close: true
+        })
       })
       
-      p.endShape()
+      p.fill(0)
+       drawRibbon(p, side.eye[0], side.eye[1], {
+         close: true,
+         curve: true
+       })
     })
-    
-    
 
+  
   },
 };
