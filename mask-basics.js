5d0cb5f87cf583eda5983be4b368e448d88d3cba
Glitch (glitch-hello-website) <none>
2022-11-16 14:47:33 +0000

    🛫🍬 Checkpoint ./mask-basics.js:270926/6646

--- mask-basics.js
+++ mask-basics.js
@@ -5,68 +5,97 @@ allMasks["basics"] = {
   description: "showing contours",
   setup() {
     // p.clear()
-    
   },
 
   draw(p, face) {
-    p.clear()
+    p.clear();
     // p.background(0, 0, 0, .01);
     let t = p.millis() * 0.001;
-    
-//     Vector2D
-    p.circle(...face.nose, 4)
-    p.circle(...face.top, 4)
-    p.circle(...face.bottom, 4)
-    
-//     face.mouth an array of contours, 
+
+    //     Vector2D
+    p.circle(...face.nose, 4);
+    p.circle(...face.top, 4);
+    p.circle(...face.bottom, 4);
+
+    //     face.mouth an array of contours,
     // each of which is an array of Vecto2D
-//     face.mouth.forEach((contour, index) => {
-//       p.fill(index*50, 100, 50)
-//       p.beginShape()
-//       contour.forEach(pt => {
-//         // p.circle(...pt, 4)
-//         // p.text("❤️", ...pt)
-//         p.vertex(...pt)
-//       })
-      
-//       p.endShape()
-//     })
-    
+    //     face.mouth.forEach((contour, index) => {
+    //       p.fill(index*50, 100, 50)
+    //       p.beginShape()
+    //       contour.forEach(pt => {
+    //         // p.circle(...pt, 4)
+    //         // p.text("❤️", ...pt)
+    //         p.vertex(...pt)
+    //       })
+
+    //       p.endShape()
+    //     })
+
     // Using drawPoints
-    p.stroke(0, 100, 50)
-    p.noFill()
-    p.strokeWeight(3)
-    p.beginShape()
-    drawPoints(p,face.mouth[2].slice(12, 20), {
+    p.stroke(0, 100, 50);
+    p.noFill();
+    p.strokeWeight(3);
+    p.beginShape();
+    drawPoints(p, face.mouth[2].slice(12, 20), {
       // close: true
-    })
-    p.vertex(...face.bottom)
-    p.endShape(p.CLOSE)
-    
-    drawContour(p, face.centerLine)
-    
-// SIDES!
+    });
+    p.vertex(...face.bottom);
+    p.endShape(p.CLOSE);
+
+    drawContour(p, face.centerLine);
+
+    // SIDES!
     face.sides.forEach((side, sideIndex) => {
-      
       // Inside a side
       // p.strokeWeight(4)
       // p.circle(...side.eyeCenter, 5)
       // p.strokeWeight(1)
-      side.face.forEach((contour, index) => {
-        let hue = (sideIndex*100 + 50*index)%360
-        p.fill(hue, 100, 50, 1)
-        drawContour(p, contour, {
-          close: true
-        })
-      })
-      
-      p.fill(0)
-       drawRibbon(p, side.eye[0], side.eye[1], {
-         close: true,
-         curve: true
-       })
-    })
+      //       side.face.forEach((contour, index) => {
 
-  
+      //         let hue = (sideIndex*100 + 10*index)%360
+      //         p.fill(hue, 100, 50 - 10*index, 1)
+      //         p.stroke(hue, 100, 100 - 10*index, 1)
+      //         drawContour(p, contour, {
+      //           close: true
+      //         })
+      //       })
+
+      p.stroke(0);
+      p.noFill();
+      //        drawRibbon(p, side.eye[0], side.eye[1], {
+      //          close: true,
+      //          curve: true,
+
+      //        })
+
+      //        drawContour(p, side.eye[0], {
+      //          close: true,
+      //          curve: true,
+
+      //          // lerpToPoint: side.eyeCenter
+      //        })
+
+      p.strokeWeight(0.4);
+      for (var i = 0; i < 10; i++) {
+        drawContour(p, side.eye[0], {
+          close: true,
+          curve: true,
+
+          // a number
+          // lerpPct: -.4*i,
+
+          // a function
+          lerpPct(ptIndex, pct, pt) {
+            return (-0.4 * i + 0.2 * Math.sin(ptIndex + t * 2)) * 0.3;
+          },
+
+          // lerpToPoint: side.eyeCenter,
+          lerpToPoint: face.top,
+        });
+      }
+
+      p.fill(0);
+      drawContour(p, side.eye[4], {});
+    });
   },
 };
