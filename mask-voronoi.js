2712ab8f32b1024f34c74305340f9a3756afcc71
Glitch (glitch-hello-website) <none>
2022-11-23 01:40:02 +0000

    🌉💶 Checkpoint ./index.html:270926/81 ./utilities.js:270926/4158 ./mask-voronoi.js:270926/6020

--- mask-voronoi.js
+++ mask-voronoi.js
@@ -6,7 +6,7 @@ allMasks["voronoi"] = {
   setup(p) {
     this.voronoi = new Voronoi();
     this.bbox = { xl: 0, xr: p.width, yt: 0, yb: p.height }; // xl is x-left, xr is x-right, yt is y-top, and yb is y-bottom
-    
+
     // a 'vertex' is an object exhibiting 'x' and 'y' properties. The
     // Voronoi object will add a unique 'voronoiId' property to all
     // sites. The 'voronoiId' can be used as a key to lookup the associated cell
@@ -16,73 +16,66 @@ allMasks["voronoi"] = {
     let sites = [
       { x: 200, y: 200 },
       { x: 240, y: 20 },
-       { x: 120, y: 240 },
+      { x: 120, y: 240 },
       { x: 50, y: 250 },
       { x: 400, y: 100 } /* , ... */,
     ];
 
     this.diagram = this.voronoi.compute(sites, this.bbox);
-    console.log(this.diagram)
+    console.log(this.diagram);
   },
 
   draw(p, face) {
     // console.log(this.bbox)
-    p.clear()
+    p.clear();
     // p.background(0, 0, 0, );
-    
-//     Compute a new diagram
-     let t = p.millis() * 0.001;
-    let sites = []
-    let count = 20
+
+    //     Compute a new diagram
+    let t = p.millis() * 0.001;
+    let sites = [];
+    let count = 20;
     for (var j = 0; j < 1; j++) {
       for (var i = 0; i < count; i++) {
-        let v = new Vector2D()
-        let r = 40 + j*50 + 100*p.noise(i)
-        let theta = (i + Math.sin(j + t))*Math.PI*2/count
-        v.setToPolar(r, theta)
-        v.add(p.width/2, p.height/2)
+        let v = new Vector2D();
+        let r = 40 + j * 50 + 100 * p.noise(i);
+        let theta = ((i + Math.sin(j + t)) * Math.PI * 2) / count;
+        v.setToPolar(r, theta);
+        v.add(p.width / 2, p.height / 2);
         // Convert to {x,y}
         // sites.push({x:v[0], y:v[1]})
       }
     }
-    
-// Add face points
+
+    // Add face points
     // face.points.forEach(v => {
     //    sites.push({x:v[0], y:v[1]})
     // })
-    face.sides.forEach(side => {
-      side.eye[0].forEach(v => {
-        sites.push({x:v[0], y:v[1]})
-      })
-    })
-    
+    face.sides.forEach((side) => {
+      side.eye[0].forEach((v) => {
+        sites.push({ x: v[0], y: v[1], pt: v });
+      });
+    });
+
     this.diagram = this.voronoi.compute(sites, this.bbox);
-    
+
+    function getCellPoints(cell, fxn) {
+      
+    }
    
+
     // Each cell in the diagram
     this.diagram.cells.forEach((cell, cindex) => {
-      
-      let index = cell.site.voronoiId
+      let index = cell.site.voronoiId;
       // console.log(index)
-      p.stroke(0, 0, 100)
-      p.strokeWeight(2)
-      
-      p.beginShape()
-      
+      p.stroke(0, 0, 100);
+      p.strokeWeight(2);
+
+      p.beginShape();
+
       // Halfedges that define the cell
-     
-      cell.halfedges.forEach(hedge => {
-        let e = hedge.edge
-        // Two ends of the edge
-        let va = e.va
-        let vb = e.vb
-//         TODO, KATE WILL SORT THESE SO WE CAN DRAW REGION
-        // p.vertex(va.x, va.y)
-        p.line(va.x, va.y, vb.x, vb.y)
-      })
-      
-      p.endShape()
-      p.circle(cell.site.x, cell.site.y, 1)
-    })
+
+      p.endShape();
+      p.circle(cell.site.x, cell.site.y, 1);
+    });
   },
 };
